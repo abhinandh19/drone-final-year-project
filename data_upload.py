acf0be5ccfa85d20015e84389c0217d364ec0ee5
abhinandh19 <61595770+abhinandh19@users.noreply.github.com>
2020-05-25 18:47:39 -0400

    Add files via upload

---
+++ data_upload.py
@@ -0,0 +1,15 @@
+from firebase import Firebase
+
+ref = firebase.push('https://test-44899.firebaseio.com/')
+
+users_ref = ref.child('users')
+users_ref.set({
+    'alanisawesome': {
+        'date_of_birth': 'June 23, 1912',
+        'full_name': 'Alan Turing'
+    },
+    'gracehop': {
+        'date_of_birth': 'December 9, 1906',
+        'full_name': 'Grace Hopper'
+    }
+})
